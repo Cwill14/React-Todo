540b7245b9d5e71c45d153a48522e77360d9b6e2
Clark Williams <theclarkwilliams@gmail.com>
2019-05-30 14:44:42 -0600

    forgot to commit at start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// import './src/App.scss';
 import TodoForm from './components/TodoComponents/TodoForm';
 import TodoList from './components/TodoComponents/TodoList';
 
@@ -23,29 +23,35 @@ class App extends React.Component {
     super();
     this.state = {
       list: [
-        // {
-        //   task: 'Organize Garage',
-        //   id: 1528817077286,
-        //   completed: false
-        // },
-        // {
-        //   task: 'Bake Cookies',
-        //   id: 1528817084358,
-        //   completed: false
-        // }
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: true
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
       ],
       task: "",
       id: "",
       completed: ""
     };
   }
+  // toggleComplete = () => {
+
+  // }
+
   addToList = e => {
     // console.log("its working");
     e.preventDefault();
     const newTodo = {
       task: this.state.task,
-      id: this.state.id,
-      completed: this.state.completed
+      // id: this.state.id,
+      id: Date.now(),
+      // completed: this.state.completed
+      completed: false
     }
     this.setState(
       { 
@@ -56,9 +62,16 @@ class App extends React.Component {
       }
     )
   }
-  clearList = e => {
+  clearCompleted = e => {
     // console.log("its totally working");
     e.preventDefault();
+    const newList = this.state.list.filter(item => item.completed === false);
+    console.log(newList);
+    this.setState(
+      {
+        list: newList
+      }
+    )
   }
   onInput = e => {
     e.preventDefault();
@@ -74,7 +87,7 @@ class App extends React.Component {
         <TodoList taskList={this.state.list}/>
         <TodoForm 
           addList={this.addToList}
-          clearList={this.clearList}
+          clearList={this.clearCompleted}
           task={this.state.task}
           onInput={this.onInput}
         />
--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import './Todo.css';
 
 // takes in the `todo` data and displays the task to the screen.
 
-const Todo = (props) => {
+const Todo = props => {
     return (
-        <div>
+        <div 
+            className={`task${props.completed === true ? ' completed-style' : ''}`}
+            // onClick={() => props.toggleComplete()}
+        >
             {props.taskObj.task}
         </div>
     )
